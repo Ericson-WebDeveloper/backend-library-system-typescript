b571891eed1184c3ebc01ccb3797a4e60f2c62ac
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-09-12 16:45:20 +0800

    Update DateTime.ts

--- src/helper/DateTime.ts
+++ src/helper/DateTime.ts
@@ -1,13 +1,6 @@
 
 
 export const calDiffISODate = (s: Date, e: Date) => {
-    // const start = new Date(s).getTime();
-    // const end = new Date(e).getTime();
-    // const milliseconds = Math.abs(end - start).toString();
-    // const seconds = parseInt(milliseconds) / 1000;
-    // const minutes = seconds / 60;
-    // const hours = minutes / 60;
-    // const days = hours / 24;
     let date1 = new Date(s.toLocaleDateString('en-Us', {timeZone: 'Asia/Manila'}));
     let date2 = new Date(e.toLocaleDateString('en-Us', {timeZone: 'Asia/Manila'}));
     const timeDifference: number = date2.getTime() - date1.getTime();
